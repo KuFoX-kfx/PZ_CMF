23107a8a2dce6b8cc4f216ec9d2849854e0f2e4e
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-09-29 23:48:42 +0300

    Add DB file and text for create

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #pyuic5 GUI_main.ui -o GUI_main.py
+"""CREATE TABLE profiles (
+    ID   INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name STRING,
+    PGF  STRING,
+    PSF  STRING
+);
+"""
 #import lib
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QThread
@@ -6,10 +13,10 @@ import sys
 from GUI_main import Ui_main
 
 app = QtWidgets.QApplication(sys.argv)
-STRT = QtWidgets.QMainWindow()
+MAIN = QtWidgets.QMainWindow()
 ui = Ui_main()
-ui.setupUi(STRT)
-STRT.show()
+ui.setupUi(MAIN)
+MAIN.show()
 
 
 
