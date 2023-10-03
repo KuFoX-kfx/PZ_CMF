55ac0c568d0956b93437dfda4d324efbec3798ac
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-10-03 19:06:34 +0300

    Add func. to select patches and btnbox func

--- main.py
+++ main.py
@@ -53,11 +53,6 @@ def LoadAllProfiles():
         except:
             pass
         ui_main.CMBox_Profile.addItems(list(map(str, db.GetProfilesName())))
-        
-def CeateProfileUI():
-    MAIN.setEnabled(0)
-    CRT.exec()
-    MAIN.setEnabled(1)
     
 def CreateDBFile():
     db.CreateDBProfiles()
@@ -72,15 +67,23 @@ def check():
         if msgBox.exec_() == QMessageBox.Yes:
             db.CreateDBProfiles()
 
+def SelectSaveGame():
+    ui_CRT.LNEdit_PTYSG.setText(QtWidgets.QFileDialog.getExistingDirectory())
+
+def SelectFolderGame():
+    ui_CRT.LNEdit_PTYG.setText(QtWidgets.QFileDialog.getExistingDirectory())
 
 
 
 
 
+ui_CRT.BTNBOX.accepted.connect(lambda: print("123"))
+ui_CRT.BTNBOX.rejected.connect(lambda: print("123123"))
 
 
-
-ui_main.PSHBTN_Create.clicked.connect(CeateProfileUI)
+ui_CRT.PSHBTN_SYSG.clicked.connect(SelectSaveGame)
+ui_CRT.PSHBTN_SYG.clicked.connect(SelectFolderGame)
+ui_main.PSHBTN_Create.clicked.connect(CRT.exec)
 ui_main.PSHBTN_Load.clicked.connect(LoadAllProfiles)
 ui_main.ACT_CreateDBF.changed.connect(CreateDBFile)
 check()
